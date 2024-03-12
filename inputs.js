c1f358017ab3e55c5f40d3206e7fac6cb743bfb9
herbertguzman <herbertguzman92@gmail.com>
2024-03-12 11:58:26 -0600

    actuliza inputs.js

--- inputs.js
+++ inputs.js
@@ -1,10 +1,18 @@
-let txtIngresado = '';
+let txtAEncriptar = '';
 
+const matrizReemplazos = [
+    {original: /a/gi, remplazo: "rea"},
+    {original: /e/gi, remplazo: "foe"},
+    {original: /i/gi, remplazo: "goi"},
+    {original: /o/gi, remplazo: "tuo"},
+    {original: /u/gi, remplazo: "pau"},
+];
 
 function IntentoEncriptar() {
     let txtArea = document.getElementById('textoDeUsuario');
-    txtIngresado = txtArea.value;
+    txtAEncriptar = txtArea.value;
 
-    console.log("El texto ingresado es", txtIngresado);
+    matrizReemplazos.forEach(({original, remplazo}) =>{txtAEncriptar.replace(original, remplazo);});
+
+    console.log(txtAEncriptar);
 }
-
